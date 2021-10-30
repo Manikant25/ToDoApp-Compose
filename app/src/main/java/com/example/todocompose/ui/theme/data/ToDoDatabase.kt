fa82c1c447f2284f8e82ae2759e57c665d1cdb85
Manikant Rai <manikant.rai2000@gmail.com>
2021-10-30 14:41:08 +0530

    hilt-setUp

--- app/src/main/java/com/example/todocompose/ui/theme/data/ToDoDatabase.kt
+++ app/src/main/java/com/example/todocompose/ui/theme/data/ToDoDatabase.kt
@@ -7,5 +7,5 @@ import com.example.todocompose.ui.theme.data.models.ToDoTask
 @Database(entities = [ToDoTask::class],version = 1,exportSchema = false)
 abstract class ToDoDatabase : RoomDatabase() {
 
-    abstract fun ToDoDao() : ToDoDao
+    abstract fun toDoDao() : ToDoDao
 }
